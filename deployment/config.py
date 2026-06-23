8cbe95af3126e1bfa41fb82507eef3f889cd54d8
Sakias Mana <sakiasmana0802@gmail.com>
2026-06-23 19:53:28 +1000

    Configure PostgreSQL database

--- deployment/config.py
+++ deployment/config.py
@@ -4,9 +4,13 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "mysql+pymysql://root:root@localhost/xray_hospital"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    if SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgresql://",
+            "postgresql+psycopg2://",
+            1
+        )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
